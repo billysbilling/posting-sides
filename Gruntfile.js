2a86779665a3c0fbbc5e51b5df4c9935b25029e8
Adam Robertson <adam@arcreative.net>
2014-01-23 14:33:21 -0800

    Files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        billy_builder: {
+            title: 'billy-posting-sides'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
---
+++ billy-posting-sides.js
@@ -0,0 +1,37 @@
+debit = Ember.Object.create({
+    value: 'debit',
+    isDebit: true,
+    isCredit: false,
+    toString: function() {
+        return '<PostingSide.debit>';
+    }
+});
+credit = Ember.Object.create({
+    value: 'credit',
+    isDebit: false,
+    isCredit: true,
+    toString: function() {
+        return '<PostingSide.credit>';
+    }
+});
+debit.opposite = credit;
+credit.opposite = debit;
+
+function resolveSide(side) {
+    if (side === debit || side === credit) {
+        return side;
+    }
+    if (side === 'debit') {
+        return debit;
+    }
+    if (side === 'credit') {
+        return credit;
+    }
+    throw new Error('Invalid side `'+side+'`');
+}
+
+module.exports = {
+    debit: debit,
+    credit: credit,
+    resolveSide: resolveSide
+};
